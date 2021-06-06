00155493683e40ff9eab916f0feab45329fa2922
Raveendran Peravene <peravene@yahoo.de>
2021-06-06 16:26:42 +0200

    use also left and right image

--- load.py
+++ load.py
@@ -23,19 +23,41 @@ def main():
     images = []
     measurements = []
     for line in lines:
+        #center image
         source_path = line[0]
         filename = source_path.split('/')[-1]
         current_path = './' + args.image_folder + '/IMG/' + filename
         image = cv2.imread(current_path)
         images.append(image)
+        #current steering
         measurement = float(line[3])
         measurements.append(measurement)
-        #flip also the image
+
+        #flip also the center image
         images.append(cv2.flip(image,1))
         measurements.append(measurement*-1)
 
+        # left image
+        source_path = line[1]
+        filename = source_path.split('/')[-1]
+        current_path = './' + args.image_folder + '/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+
+        # right image
+        source_path = line[2]
+        filename = source_path.split('/')[-1]
+        current_path = './' + args.image_folder + '/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+
+        # create adjusted steering measurements for the side camera images
+        correction = 0.2 # this is a parameter to tune
+        measurements.append(measurement + correction) # Left
+        measurements.append(measurement - correction) # right
+
     data = {'X_train':  np.array(images), 'y_train': np.array(measurements)}
-    pickle.dump(data, open( './' + args.image_folder + '/dataAug.pickle', "wb" ))
+    pickle.dump(data, open( './' + args.image_folder + '/dataAug.pickle', "wb" ), protocol=4)
 
 if __name__ == '__main__':
     main()
